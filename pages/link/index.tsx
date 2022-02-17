0f110628f06e9e25ba9860f547116b3dbcbe7c06
1ncursio <ckswn1323@naver.com>
2022-02-17 16:42:28 +0900

    [ADD] link 페이지

---
+++ pages/link/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LinkPage = () => {
+  return (
+    <div>
+      <div className="p-2 bg-gray-100 grid grid-cols-3 grid-rows-4">
+        <button className="bg-white rounded-full font-bold">1</button>
+        <button className="bg-white rounded-full font-bold">2</button>
+        <button className="bg-white rounded-full font-bold">3</button>
+        <button className="bg-white rounded-full font-bold">4</button>
+        <button className="bg-white rounded-full font-bold">5</button>
+        <button className="bg-white rounded-full font-bold">6</button>
+        <button className="bg-white rounded-full font-bold">7</button>
+        <button className="bg-white rounded-full font-bold">8</button>
+        <button className="bg-white rounded-full font-bold">9</button>
+        <button className="bg-white rounded-full font-bold">ABC</button>
+        <button className="bg-white rounded-full font-bold">0</button>
+        <button>⌫</button>
+      </div>
+    </div>
+  );
+};
+
+export default LinkPage;
