dbaaea10dc0b7a04efee683346839fbd09591855
1ncursio <ckswn1323@naver.com>
2022-02-17 16:43:00 +0900

    [Add] i18n 설정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["via.placeholder.com"],
+    domains: ['via.placeholder.com'],
+  },
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'ko-KR', 'ja-JP'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'ko-KR',
   },
 };
 
