a30daeb473b0d0d27a5470acc9d32607f7282737
1ncursio <ckswn1323@naver.com>
2022-02-17 18:53:43 +0900

    [Add] react-hook-form and useInput

--- pages/link/index.tsx
+++ pages/link/index.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LinkPage = () => {
+  const { register, handleSubmit, reset, getValues } = useForm();
+  const elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'ABC', 0, '⌫'];
+
+  const onSubmitCode = (data) => {
+    console.log(data);
+  };
+
+  const onClickButton = (element) => (e: React.MouseEvent) => {
+    switch (element) {
+      case '⌫':
+        reset({
+          code: getValues().code.slice(0, -1),
+        });
+        break;
+      default:
+        reset({
+          code: getValues().code + element,
+        });
+        break;
+    }
+  };
+
   return (
     <div>
+      <form onSubmit={handleSubmit(onSubmitCode)}>
+        <input
+          {...register('code', {
+            required: true,
+            maxLength: 4,
+          })}
+        />
+      </form>
       <div className="p-2 bg-gray-100 grid grid-cols-3 grid-rows-4">
-        <button className="bg-white rounded-full font-bold">1</button>
-        <button className="bg-white rounded-full font-bold">2</button>
-        <button className="bg-white rounded-full font-bold">3</button>
-        <button className="bg-white rounded-full font-bold">4</button>
-        <button className="bg-white rounded-full font-bold">5</button>
-        <button className="bg-white rounded-full font-bold">6</button>
-        <button className="bg-white rounded-full font-bold">7</button>
-        <button className="bg-white rounded-full font-bold">8</button>
-        <button className="bg-white rounded-full font-bold">9</button>
-        <button className="bg-white rounded-full font-bold">ABC</button>
-        <button className="bg-white rounded-full font-bold">0</button>
-        <button>⌫</button>
+        {elements.map((element) => (
+          <button
+            key={element}
+            type="button"
+            onClick={onClickButton(element)}
+            className="bg-white rounded-full font-bold"
+          >
+            {element}
+          </button>
+        ))}
       </div>
     </div>
   );
