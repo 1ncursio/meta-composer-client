52564f7f44696c379a9e77742aec316686d108b7
1ncursio <ckswn1323@naver.com>
2022-02-22 17:02:57 +0900

    [Fix] 타입 가드

--- pages/[roomId]/vr-room/styles.ts
+++ pages/[roomId]/vr-room/styles.ts
@@ -1,6 +1,7 @@
 import { classnames } from 'tailwindcss-classnames';
 
-export const icon = (isOculus) =>
+export const icon = (isOculus: boolean) =>
+  // @ts-ignore
   classnames({
     'text-base-300': isOculus,
   });
