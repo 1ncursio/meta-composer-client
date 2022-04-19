28ffa4227f82878a66efe392a5533ca2f813bf39
jungseKim <bible6146@naver.com>
2022-04-19 16:26:06 +0900

    레슨생성버튼 추가

--- pages/lessons/index.tsx
+++ pages/lessons/index.tsx
@@ -1,3 +1,4 @@
+import useUserSWR from '@hooks/swr/useUserSWR';
 import fetcher from '@lib/api/fetcher';
 import LessonComponent from '@react-components/lessonComponents';
 import ILesson from '@typings/ILesson';
@@ -8,6 +9,7 @@ import useSWR from 'swr';
 import useSWRInfinite from 'swr/infinite';
 const LessonsIndexPage = () => {
   // const { data: LessonData } = useSWR<ILesson[]>('/lessons', fetcher);
+  const { data: userData } = useUserSWR();
   const {
     data: LessonDataList,
     mutate: mutateLessonData,
@@ -36,6 +38,13 @@ const LessonsIndexPage = () => {
 
   return (
     <div className="relative h-full ">
+      <div className="flex flex-col items-end mt-4">
+        {userData?.teacher && (
+          <Link href="/lessons/createLesson">
+            <a className="bg-primary p-1 rounded font-bold text-sm hover:bg-gray-500">레슨생성</a>
+          </Link>
+        )}
+      </div>
       <div className=" grid grid-cols-4 grid-rows-2 grid-flow-col h-full   m-10   ">
         {LessonDataList &&
           LessonDataList[current - 1]?.map((lesson) => {
