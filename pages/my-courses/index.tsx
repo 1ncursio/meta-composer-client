adc1c4b12a5059a3cc51438ae1b3ba577a9eeb4f
jungseKim <bible6146@naver.com>
2022-05-06 19:17:47 +0900

    dashborder first

--- pages/my-courses/index.tsx
+++ pages/my-courses/index.tsx
@@ -12,63 +12,134 @@ import {
   ViewSwitcher,
   AppointmentTooltip,
   AppointmentForm,
+  MonthView,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import fetcher from '@lib/api/fetcher';
 import useSWR from 'swr';
 import { useEffect } from 'react';
 import ISignup from '@typings/ISignup';
 import { sign } from 'crypto';
-
+import dayjs from 'dayjs';
+import { json } from 'stream/consumers';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import Image from 'next/image';
 interface iDate {
-  startDate: Date;
-  // endDate: string;
+  startDate: string;
+  endDate: string;
   title: string;
 }
 
 const MyCoursesPage = () => {
-  const { data, mutate: mutateLessonData } = useSWR<ISignup[]>('/signup-timetables', fetcher);
-
+  const { data: signUpData, mutate: mutateLessonData } = useSWR<ISignup[]>('/signup-timetables', fetcher);
+  const [viewPage, setViewPage] = useState(1);
   const sData = useMemo(() => {
-    if (!data) return;
+    if (!signUpData) return;
+    console.log(signUpData);
     const result: iDate[] = [];
-    data.forEach((signup) => {
+    signUpData.forEach((signup) => {
       signup.signuptimetables.forEach((ti) => {
+        const date = new Date(ti.time);
+        // console.log(date.getMonth(), date.getDate(), date.getHours());
         result.push({
-          startDate: ti.time,
-          // endDate:new Date(ti.time).get
-          title: signup.__lesson__.name,
+          startDate: date.toString(),
+          endDate: new Date(date.setHours(date.getHours() + 2)).toString(),
+          title: signup.__lesson__.name + ' 수업입니다',
         });
       });
     });
     return result;
-  }, [data]);
-  useEffect(() => {
-    console.log(data, '!!!');
-  }, [data]);
-  const currentDate = new Date();
-  const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-  ];
+  }, [signUpData]);
+
+  const pageChange = (id: number) => {
+    setViewPage(id);
+    console.log(id);
+  };
+
   return (
     <DashboardContainer>
-      <div className="container w-2/3 items-center">
-        <Scheduler data={sData} height={640}>
-          <ViewState currentDate={currentDate} defaultCurrentViewName="Week" />
-          <DayView startDayHour={9} endDayHour={14} />
-          <WeekView startDayHour={9} endDayHour={14} />
-          <Toolbar />
-          <ViewSwitcher />
-          <Appointments />
-          <AppointmentTooltip
-            // headerComponent={() => <div> 상세히</div>}
-            showCloseButton
-            showOpenButton
-            // layoutComponent={() => <div className="h-20"></div>}
-            contentComponent={() => <div>heelow</div>}
-            // commandButtonComponent={() => <div>hi</div>}
-          />
-        </Scheduler>
+      <div className="container w-2/3 items-center border p-2 mt-10">
+        <div className="tabs border-b-2  flex justify-center ">
+          <button onClick={() => pageChange(1)} className="tab text-black font-bold">
+            시간표
+          </button>
+          <button onClick={() => pageChange(2)} className="tab text-black font-bold">
+            수강 목록
+          </button>
+          {/* <p className="tab tab-active">Tab 2</p>
+        <p className="tab">Tab 3</p> */}
+        </div>
+        {viewPage == 1 ? (
+          <Scheduler data={sData} height={640}>
+            <ViewState currentDate={new Date()} defaultCurrentViewName="Month" />
+            <DayView startDayHour={12} endDayHour={18} />
+            <WeekView startDayHour={12} endDayHour={18} />
+            <MonthView />
+            <Toolbar />
+            <ViewSwitcher />
+            <Appointments />
+            <AppointmentTooltip
+              // headerComponent={() => <div> 상세히</div>}
+
+              // layoutComponent={() => <div className="h-20"></div>}
+              // contentComponent={(sd) => <div>{JSON.stringify(sd)}</div>}
+              showCloseButton
+              showOpenButton
+              // commandButtonComponent={() => <div>hi</div>}
+            />
+          </Scheduler>
+        ) : (
+          <div className="flex-col w-full ">
+            {signUpData?.map((signup) => {
+              return (
+                <div className="w-full flex-col  p-2 gap-2 border">
+                  <div className="flex gap-2 items-end">
+                    <Image src={signup.__lesson__.imageURL} width={60} height={60} />
+                    <p className="text-lg font-bold ">{signup.__lesson__.name}</p>
+                  </div>
+                  <div className="flex gap-8">
+                    <div className="hidden lg:w-1/2 lg:flex lg:justify-between border-t">
+                      <div className="w-20 flex-col font-bold">
+                        수업 진행률
+                        <CircularProgressbar value={30} text={'30%'} strokeWidth={10} />
+                      </div>
+                      <div className="w-20 flex-col font-bold">
+                        과제 진행률
+                        <CircularProgressbar
+                          value={30}
+                          text={'30%'}
+                          styles={buildStyles({
+                            pathColor: `#f88`,
+                            textColor: '#f88',
+                            trailColor: '#d6d6d6',
+                            backgroundColor: '#3e98c7',
+                          })}
+                        />
+                      </div>
+                      <div className="w-20 flex-col font-bold">
+                        수업 진행률
+                        <CircularProgressbar value={30} text={'30%'} strokeWidth={10} />
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      {signup.__lesson__.__teacher__.user.profile_image ? (
+                        <Image
+                          className="rounded-full w-10 h-10 object-cover"
+                          src={signup.__lesson__.__teacher__.user.profile_image}
+                          width={60}
+                          height={60}
+                        />
+                      ) : (
+                        <></>
+                      )}
+                      <p>{signup.__lesson__.__teacher__.user.username}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </DashboardContainer>
   );
--- react-components/DashboardMain/index.tsx
+++ react-components/DashboardMain/index.tsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import useUserSWR from '@hooks/swr/useUserSWR';
+import fetcher from '@lib/api/fetcher';
+import ISignup from '@typings/ISignup';
+import optimizeImage from '@utils/optimizeImage';
+import dayjs from 'dayjs';
+import Image from 'next/image';
+import React, { useEffect, useMemo } from 'react';
+import useSWR from 'swr';
 
 const DashboardMain = () => {
-  return <div>DashboardMain</div>;
+  const { data: userData } = useUserSWR();
+  const { data: signUpData, mutate: mutateLessonData } = useSWR<ISignup[]>('/signup-timetables', fetcher);
+  useEffect(() => {
+    if (!signUpData) return;
+    const row = new Date();
+    let max = new Date(row.getFullYear() + 2, 1, 1);
+    let closeSignup = null;
+    signUpData?.forEach((signup) => {
+      signup.signuptimetables.forEach((table) => {
+        const time = new Date(table.time);
+        if (row < time && max > time) {
+          max = time;
+          closeSignup = signup;
+        }
+      });
+    });
+    console.log(closeSignup);
+  }, [signUpData]);
+
+  const viewSingup = useMemo(() => {
+    if (!signUpData) return;
+    const row = new Date();
+    let max = new Date(row.getFullYear() + 2, 1, 1);
+    let closeSignup = signUpData[0];
+    signUpData?.forEach((signup) => {
+      signup.signuptimetables.forEach((table) => {
+        const time = new Date(table.time);
+        if (row < time && max > time) {
+          max = time;
+          closeSignup = signup;
+        }
+      });
+    });
+    // console.log((max.getTime() - row.getTime()) / (1000 * 60 * 60 * 24));
+
+    const lesson = closeSignup.signuptimetables.length;
+    // const finishedCount = closeSignup.signuptimetables.filter((table) => {
+    //   return new Date(table.time) < row;
+    // }).length;
+    const finishedCount = 40;
+    return { closeSignup, lesson, finishedCount, date: `${max.getMonth() + 1}월 ${max.getDate()}일` };
+  }, [signUpData]);
+
+  return (
+    <div className="w-full h-full">
+      <div className="w-full h-12 bg-gray-600 text-white font-bold text-xl flex items-center pl-10 mb-6">대시보드</div>
+      <div className="grid grid-cols-2 mx-auto  w-full lg:w-2/3 h-full gap-x-2 gap-y-2 ">
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2">
+          <p className="font-bold text-md">😀{userData?.username}</p>
+          <div className="flex h-20 w-full gap-2">
+            {userData?.profile_image && (
+              <Image
+                src={userData!.profile_image}
+                alt="user profile"
+                //   placeholder="blur"
+                // layout=''
+                width={100}
+                height={64}
+                className="mask mask-squircle"
+              />
+            )}
+            <p className="font-bold text-lg ">{userData?.username} 님, 오늘 하루 화이팅!</p>
+          </div>
+          <div className="h-full  w-full flex flex-col-reverse">
+            <div className="w-full flex flex-row-reverse">
+              <button className=" text-gray-400">프로필 수정하기 </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between">
+          <p className="font-bold text-md">📖다음 레슨</p>
+          <div className="flex items-end gap-2">
+            <p className="font-bold text-md">{viewSingup?.closeSignup.__lesson__.name}</p>
+            <p className=" text-xs">({viewSingup?.date})</p>
+          </div>
+          <div className="w-full">
+            <p>
+              진도율: {'   '}
+              {viewSingup!.finishedCount}/{viewSingup!.lesson}
+              {'   '}({(viewSingup!.finishedCount / viewSingup!.lesson) * 100}%)
+            </p>
+
+            <progress
+              className="progress w-3/4"
+              value={(viewSingup!.finishedCount / viewSingup!.lesson) * 100}
+              max="100"
+            ></progress>
+
+            <div className="w-full flex flex-row-reverse">
+              <button className="btn btn-error btn-sm">레슨 하러가기</button>
+              <button className="btn btn-success btn-sm">내모든 레슨</button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between">
+          <p className="font-bold text-md">📝최근 내 노트</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardMain;
