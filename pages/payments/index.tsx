e2320e2b45ff04318364c17a1b839f873952b5ca
jungseKim <bible6146@naver.com>
2022-05-09 20:29:07 +0900

    dashborder finish

--- pages/payments/index.tsx
+++ pages/payments/index.tsx
@@ -1,10 +1,62 @@
+import fetcher from '@lib/api/fetcher';
 import DashboardContainer from '@react-components/DashboardContainer';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React from 'react';
+import Image from 'next/image';
+import React, { useCallback, useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 const PaymentsIndexPage = () => {
-  return <DashboardContainer>PaymentsIndexPage</DashboardContainer>;
+  const { data, mutate } = useSWR('/payments', fetcher);
+  const [num, setNum] = useState(false);
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  const chek = useCallback(() => {
+    setNum((before) => !before);
+  }, []);
+  return (
+    <DashboardContainer>
+      <div className="w-full h-full">
+        <div className="w-full h-12 bg-gray-600 text-white font-bold text-xl flex items-center pl-10 mb-6">
+          결제내역
+        </div>
+        <div className="w-4/5 lg:w-1/2 mx-auto flex flex-col  ">
+          <div className="border flex flex-col w-full p-2 rounded-md">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-3">
+                <Image
+                  src="https://images.unsplash.com/photo-1513269798455-b0c23c9e4d5b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8cGlhbm98fHx8fHwxNjUxNTU2NzU3&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+                  alt="user profile"
+                  //   placeholder="blur"
+                  // layout="fill"
+                  width={64}
+                  height={64}
+                  className="mask mask-squircle"
+                />
+                <div className="flex flex-col">
+                  <p className="font-bold text-lg">클래식 레슨 </p>
+                  <p className="font-bold text-lg">김정세</p>
+                </div>
+              </div>
+              <div onClick={() => chek()} className="border rounded-xl  bg-blue-500 font-bold text-white p-1 pl-2 pr-2">
+                레슨중
+              </div>
+            </div>
+            <div className="w-full flex flex-row-reverse">
+              <div className="flex flex-col">
+                <p className="font-bold">2022.12.02 | 수업시작</p>
+                <p className="font-bold">2022.12.02 | 수업종료</p>
+              </div>
+            </div>
+            <div className={num ? 'mt-2 border-t-2 w-full flex justify-between h-20' : 'hidden '}>
+              <p className="font-bold"></p>ㅌ
+            </div>
+          </div>
+        </div>{' '}
+      </div>
+    </DashboardContainer>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
--- react-components/DashboardMain/index.tsx
+++ react-components/DashboardMain/index.tsx
@@ -4,31 +4,18 @@ import ISignup from '@typings/ISignup';
 import optimizeImage from '@utils/optimizeImage';
 import dayjs from 'dayjs';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useMemo } from 'react';
 import useSWR from 'swr';
 
 const DashboardMain = () => {
   const { data: userData } = useUserSWR();
   const { data: signUpData, mutate: mutateLessonData } = useSWR<ISignup[]>('/signup-timetables', fetcher);
-  useEffect(() => {
-    if (!signUpData) return;
-    const row = new Date();
-    let max = new Date(row.getFullYear() + 2, 1, 1);
-    let closeSignup = null;
-    signUpData?.forEach((signup) => {
-      signup.signuptimetables.forEach((table) => {
-        const time = new Date(table.time);
-        if (row < time && max > time) {
-          max = time;
-          closeSignup = signup;
-        }
-      });
-    });
-    console.log(closeSignup);
-  }, [signUpData]);
 
   const viewSingup = useMemo(() => {
-    if (!signUpData) return;
+    if (!signUpData || signUpData?.length < 1)
+      return { closeSignup: null, lesson: 0, finishedCount: 0, date: `아직 없습니다` };
+
     const row = new Date();
     let max = new Date(row.getFullYear() + 2, 1, 1);
     let closeSignup = signUpData[0];
@@ -41,21 +28,21 @@ const DashboardMain = () => {
         }
       });
     });
-    // console.log((max.getTime() - row.getTime()) / (1000 * 60 * 60 * 24));
 
     const lesson = closeSignup.signuptimetables.length;
-    // const finishedCount = closeSignup.signuptimetables.filter((table) => {
-    //   return new Date(table.time) < row;
-    // }).length;
-    const finishedCount = 40;
+    const finishedCount = closeSignup.signuptimetables.filter((table) => {
+      return new Date(table.time) < row;
+    }).length;
+
     return { closeSignup, lesson, finishedCount, date: `${max.getMonth() + 1}월 ${max.getDate()}일` };
   }, [signUpData]);
 
   return (
-    <div className="w-full h-full">
+    <div className="w-4/5 h-full">
       <div className="w-full h-12 bg-gray-600 text-white font-bold text-xl flex items-center pl-10 mb-6">대시보드</div>
-      <div className="grid grid-cols-2 mx-auto  w-full lg:w-2/3 h-full gap-x-2 gap-y-2 ">
-        <div className="w-full h-48 border flex flex-col gap-y-2 p-2">
+      <div className="grid grid-cols-2 mx-auto  w-full lg:w-2/3 h-full gap-x-4 gap-y-4 ">
+        {/* 프로필 */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 rounded">
           <p className="font-bold text-md">😀{userData?.username}</p>
           <div className="flex h-20 w-full gap-2">
             {userData?.profile_image && (
@@ -63,47 +50,87 @@ const DashboardMain = () => {
                 src={userData!.profile_image}
                 alt="user profile"
                 //   placeholder="blur"
-                // layout=''
-                width={100}
+                // layout="fill"
+                width={64}
                 height={64}
                 className="mask mask-squircle"
               />
             )}
             <p className="font-bold text-lg ">{userData?.username} 님, 오늘 하루 화이팅!</p>
           </div>
-          <div className="h-full  w-full flex flex-col-reverse">
+          <div className="h-full  w-full flex flex-col-reverse ">
             <div className="w-full flex flex-row-reverse">
-              <button className=" text-gray-400">프로필 수정하기 </button>
+              <Link href="/my-profile">
+                <a className=" text-gray-400">프로필 수정하기</a>
+              </Link>
+              {/* <button className=" text-gray-400">프로필 수정하기 </button> */}
             </div>
           </div>
         </div>
-        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between">
+        {/* 바로다음 레슨 */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between rounded">
           <p className="font-bold text-md">📖다음 레슨</p>
           <div className="flex items-end gap-2">
-            <p className="font-bold text-md">{viewSingup?.closeSignup.__lesson__.name}</p>
+            <p className="font-bold text-md">
+              {viewSingup.closeSignup ? viewSingup.closeSignup?.__lesson__.name : '아직 없습니다'}
+            </p>
             <p className=" text-xs">({viewSingup?.date})</p>
           </div>
           <div className="w-full">
-            <p>
-              진도율: {'   '}
-              {viewSingup!.finishedCount}/{viewSingup!.lesson}
-              {'   '}({(viewSingup!.finishedCount / viewSingup!.lesson) * 100}%)
-            </p>
+            {viewSingup && (
+              <p>
+                진도율: {'   '}
+                {viewSingup.finishedCount}/{viewSingup.lesson}
+                {'   '}({(viewSingup.finishedCount / viewSingup!.lesson) * 100}%)
+              </p>
+            )}
 
-            <progress
-              className="progress w-3/4"
-              value={(viewSingup!.finishedCount / viewSingup!.lesson) * 100}
-              max="100"
-            ></progress>
+            {viewSingup && (
+              <progress
+                className="progress w-3/4"
+                value={(viewSingup.finishedCount / viewSingup!.lesson) * 100}
+                max="100"
+              ></progress>
+            )}
 
             <div className="w-full flex flex-row-reverse">
-              <button className="btn btn-error btn-sm">레슨 하러가기</button>
+              <Link href={viewSingup.closeSignup ? `/lessons/${viewSingup.closeSignup.__lesson__.id}` : `/lessons`}>
+                <a className="btn btn-error btn-sm">레슨 하러가기</a>
+              </Link>
+              {/* <button className="btn btn-error btn-sm">레슨 하러가기</button> */}
               <button className="btn btn-success btn-sm">내모든 레슨</button>
             </div>
           </div>
         </div>
-        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between">
+        {/* 내노트  */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between rounded">
           <p className="font-bold text-md">📝최근 내 노트</p>
+        </div>
+        {/* 학습통계 */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between rounded">
+          <p className="font-bold text-md">🏃🏻학습 통계</p>
+          <div className="flex w-full my-auto justify-between">
+            <div>
+              <p className="text-center text-5xl font-bold text-gray-400">0</p>
+              <p className="text-xs lg:text-base">완료 레슨수</p>
+            </div>
+            <div>
+              <p className="text-center text-5xl font-bold text-gray-400">0</p>
+              <p className="text-xs lg:text-base">완료 수업수</p>
+            </div>
+            <div>
+              <p className="text-center text-5xl font-bold text-gray-400">0</p>
+              <p className="text-xs lg:text-base">획득 수료증</p>
+            </div>
+          </div>
+        </div>
+        {/* 콩쿨 */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between rounded">
+          <p className="font-bold text-md">🏆My competition</p>
+        </div>
+        {/* 완료한 강의 */}
+        <div className="w-full h-48 border flex flex-col gap-y-2 p-2 justify-between rounded">
+          <p className="font-bold text-md">🎓완료한 강의</p>
         </div>
       </div>
     </div>
